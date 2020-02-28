28ef7d0bfad4d403f4cbd1532670583dd6eb389e
jtinianow <jermtee@gmail.com>
2020-02-28 10:32:27 -0700

    hook up color copying

--- src/app/components/App.svelte
+++ src/app/components/App.svelte
@@ -1,11 +1,36 @@
 <script>
-    export let name;
+    import '../styles/figma-plugin-ds.min.css';
+    import '../styles/ui.css';
+
+    onmessage = event => {
+        const {type, message} = event.data.pluginMessage;
+        console.log({type, message});
+
+        switch (type) {
+            case 'set-preferences':
+                console.log('set-preferences');
+                return;
+            case 'copy-color':
+                const el = document.createElement('textarea');
+                el.value = message.str;
+                document.body.appendChild(el);
+                el.select();
+                document.execCommand('copy');
+                document.body.removeChild(el);
+                parent.postMessage(
+                    {
+                        pluginMessage: {
+                            type: 'close-plugin',
+                            data: {},
+                        },
+                    },
+                    '*'
+                );
+                return;
+            default:
+                return;
+        }
+    };
 </script>
 
-<style>
-    h1 {
-        color: purple;
-    }
-</style>
-
-<h1>Hello {name}!</h1>
+<h2 className="section-title">Which formats would you like to copy?</h2>
